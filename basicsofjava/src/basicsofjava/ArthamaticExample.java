2a30319919153d17340f10b050bb05bfeeaf80e5
Venkanna <Venkanna@LAPTOP-EPO1325V>
2024-11-03 22:12:08 +0530

    secondcommit

---
+++ basicsofjava/src/basicsofjava/ArthamaticExample.java
@@ -0,0 +1,31 @@
+package basicsofjava;
+
+public class ArthamaticExample {
+
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=20;
+	//addition
+		int sum=a+b;
+		System.out.println("addition:"+sum);
+   //substraction
+		int SVP=a-b;
+		System.out.println("subtracton:"+SVP);
+  //Multiplivation
+		int svkp=a*b;
+		System.out.println("Multiplication:"+svkp);
+ //division
+		int div=a/b;
+		System.out.println("Division:"+div);
+		
+//modul
+		int modul=a%b;
+		System.out.println("modul:"+ modul);
+		
+				
+		
+
+	}
+
+}
--- basicsofjava/src/basicsofjava/ReturnExample.java
+++ basicsofjava/src/basicsofjava/ReturnExample.java
@@ -3,8 +3,9 @@ package basicsofjava;
 public class ReturnExample 
 {
 	public static void main(String[] args) {
-	  System.out.println("sum(6,10)");
+	  System.out.println(sum(6,10));
 	  {
+		  static int sum(int a,int b);
 		  
 	  }
 	  
