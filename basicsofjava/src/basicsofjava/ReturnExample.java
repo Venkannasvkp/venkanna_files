343ed612f26f58de2a63abdf3062ffd4ece99359
Venkannasvkp <venkannaakana@gmail.com>
2024-11-03 21:50:51 +0530

    Update ReturnExample.java

--- basicsofjava/src/basicsofjava/ReturnExample.java
+++ basicsofjava/src/basicsofjava/ReturnExample.java
@@ -2,12 +2,12 @@ package basicsofjava;
 
 public class ReturnExample 
 {
-	public static void main(String[] args) {
-	  System.out.println("sum(6,10)");
-	  {
-		  
-	  }
-	  
+      public static void main(String[] args) {
+		System.out.println(sum(5,10));
+		}
+	static int sum(int a,int b)
+	{
+		return a+b;
 	}
-}
 
+}
