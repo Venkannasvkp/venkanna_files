2e5ffbea7cad20e54b58ec04e7f0a6bc4fdd3d64
Venkannasvkp <venkannaakana@gmail.com>
2025-03-29 15:09:55 +0530

    Update VectorDemo.java

--- javaprogram/src/com/tnsif/capgemini/C2tcollections/VectorDemo.java
+++ javaprogram/src/com/tnsif/capgemini/C2tcollections/VectorDemo.java
@@ -14,19 +14,14 @@ public class VectorDemo {
 		Numbers.add(50);
 		Numbers.add(60);
 		
-		//we can print all the value
 		System.out.println("The total number of values are"+Numbers);
-		//we can print the size of the elements
 		System.out.println("The size of vector list is"+Numbers.size());
-		//we can modifying the value of the List we can follow
 		Numbers.set(2,90);
 		System.out.println("Nubers list after reset"+Numbers);
-		//we can check the particular element that existing in list
 		if(Numbers.contains(20))
 		{
 			System.out.println("The element are found");
 		}
-	    //we print the Elements on one order we use
 		for(Integer items:Numbers)
 		{
 			System.out.println("The Following Integers are"+items);
